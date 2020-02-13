99935fed23c6e3e4769addbc28031b8ad13d826f
James Barciz <jamesjbarciz@gmail.com>
2020-02-12 23:10:28 -0500

    create user input

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hello World!"
 
+
 @app.route("/about")
 def about():
     return "About Me"
+
+@app.route("/users")
+@app.route("/users.json")
+def users():
+    users = [
+        {"id":1, "name": "First User"},
+        {"id":2, "name": "Second User"},
+        {"id":3, "name": "Third User"},
+    ]
+    return jsonify(users)
+
+
+@app.route("/users/create", methods=['POST'])
+def create_user():
+    print('CREATING A NEW USER...')
+    print('FORM DATA:', dict(request.form))
+    # todo: create a new users
+    return jsonify({'message':'CREATED OK (TODO)'})
