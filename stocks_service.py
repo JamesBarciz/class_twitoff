376b22d405567df3b611abe7fdde0ea222e195f8
James Barciz <jamesjbarciz@gmail.com>
2020-02-19 21:25:40 -0500

    new files and packages

---
+++ stocks_service.py
@@ -0,0 +1,17 @@
+
+import json
+import requests
+
+
+request_url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSLA&apikey=123abc'
+
+response = requests.get(request_url)
+print(response)
+
+print(response.status_code)
+print(type(response.text)) #> string
+
+parsed_response = json.loads(response.text)
+print(type(parsed_response)) #> dict
+
+# TODO: work with the parsed response
